74dea551a66bab0ad96bcadda059c8f2126b31f5
Andrew0502 <agcmarcus@gmail.com>
2020-09-07 17:18:29 -0400

    Added questions in script.

--- Assets/script.js
+++ Assets/script.js
@@ -75,3 +75,51 @@ function clearStatusClass(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
 }
+
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            {text: "strings", correct: false},
+            {text: "booleans", correct: false},
+            {text: "alerts", correct: true},
+            {text: "numbers", correct: false}
+        ]
+    },
+    {
+        question: 'The condition in an if/else statement is enclosed within____.',
+        answers: [
+          { text: 'quotes', correct: false },
+          { text: 'curly brackets', correct: false},
+          { text: 'parentheses', correct: true },
+          { text: 'square brackets', correct: false }
+        ]
+      },
+      {
+        question: 'Arrays in JavaScript can be used to store _____. ',
+        answers: [
+          { text: 'numbers and strings', correct: false },
+          { text: 'other arrays', correct: false },
+          { text: 'booleans', correct: false },
+          { text: 'All of the above!', correct: true }
+        ]
+      },
+      {
+        question: 'String values must be enclosed within _____ when being assigned to variables.',
+        answers: [
+          { text: 'commas', correct: false },
+          { text: 'curly brackets', correct: false},
+          { text: 'quotes', correct: true },
+          { text: 'parentheses', correct: false }
+        ]
+      },
+      {
+      question: 'A very useful tool used to during development and debugging for printing content to the debugger is:',
+        answers: [
+          { text: 'JavaScript', correct: false },
+          { text: 'terminal/bash', correct: false},
+          { text: 'for loops', correct: true },
+          { text: 'console.log()', correct: false }
+        ]
+      }
+]
