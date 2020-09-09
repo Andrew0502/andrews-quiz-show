004cc09da9a6b8b3d7cc72ce07631710f92baa32
Andrew0502 <agcmarcus@gmail.com>
2020-09-08 20:16:05 -0400

    Started over project with new index, script, and style pages. Added boilerplate and navbar to index.

---
+++ script.js
@@ -0,0 +1,25 @@
+// This document will create a quiz game to practice 
+// 1. create a landing page.
+// 2. nav bar at top.
+// 3. high score on the left
+// 4. current score in the middle.
+// 5. timer on the right ~60 seconds. unhidden, but doesnt start until you click the start button
+// 6. div card area, centered, with bold question text.
+// 7. 4 answers, perhaps bring in the answer arrays?
+// 8.    container          
+//     <div id = answer 1> "start quiz button in this instance         
+//     <div id = answer 2> hidden         
+//     <div id = answer 3> hidden         
+//     <div id = answer 4> hidden         
+//     container     
+
+// h1> - Coding Quiz Challenge         
+// <h2> - "try and take this quiz blah blah."         
+//         
+//     <div = line> hidden        
+//      <h2>         hidden       
+//     Timer already exists at the top right hand side, but hasn't been started          
+//     Top left hand side is a high score link          
+//     Content saying there is a quiz here          
+//     a button to start      div     hidden line     answer - content  Step 2 Target ## TARGETS     id="timer" linked with countDownNumbDisp variable     
+//     id="timeStart" - button that clicks & begins quiz     id="subHeading" -
